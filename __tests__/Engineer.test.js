855771ed9ad869d1350a35d40d4515a35cc6c509
Sheev <ericmarcypro@gmail.com>
2023-01-03 18:41:08 -0500

    minor fix to engineer test, built intern test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -5,27 +5,27 @@ test('constructor returns object', () => {
     expect(typeof(example)).toBe('object');
 })
 
-test('getName returns employee name', () => {
+test('getName returns engineer name', () => {
     const Frank = new Engineer("Frank", "1", "frank@company.com", "https://github.com/franklyfrank");
     expect(Frank.getName()).toBe("Frank");
 })
 
-test('getId returns employee id', () => {
+test('getId returns engineer id', () => {
     const Steve = new Engineer("Steve", "2", "steve@company.com", "https://github.com/stevestrange");
     expect(Steve.getId()).toBe("2");
 })
 
-test('getEmail returns employee email', () => {
+test('getEmail returns engineer email', () => {
     const Dave = new Engineer("Dave", "3", "dave@company.com", "https://github.com/dangerdave");
     expect(Dave.getEmail()).toBe("dave@company.com");
 })
 
-test('getRole returns Employee', () => {
+test('getRole returns Engineer', () => {
     const Phil = new Engineer("Phil", "4", "phil@company.com", "https://github.com/phillyphil");
     expect(Phil.getRole()).toBe("Engineer");
 })
 
-test('getGithub returns employee github link', () => {
+test('getGithub returns engineer github link', () => {
     const James = new Engineer("James", "5", "james@company.com", "https://github.com/justjames");
     expect(James.getGithub()).toBe("https://github.com/justjames");
 })
---
+++ __tests__/Intern.test.js
@@ -0,0 +1,31 @@
+const Intern = require('../lib/Intern');
+
+test('constructor returns object', () => {
+    const example = new Intern();
+    expect(typeof(example)).toBe('object');
+})
+
+test('getName returns intern name', () => {
+    const Frank = new Intern("Frank", "1", "frank@company.com", "Loyola UMD");
+    expect(Frank.getName()).toBe("Frank");
+})
+
+test('getId returns intern id', () => {
+    const Steve = new Intern("Steve", "2", "steve@company.com", "Yale");
+    expect(Steve.getId()).toBe("2");
+})
+
+test('getEmail returns intern email', () => {
+    const Dave = new Intern("Dave", "3", "dave@company.com", "Princeton");
+    expect(Dave.getEmail()).toBe("dave@company.com");
+})
+
+test('getRole returns Intern', () => {
+    const Phil = new Intern("Phil", "4", "phil@company.com", "Rutgers Coding Bootcamp FSF");
+    expect(Phil.getRole()).toBe("Intern");
+})
+
+test('getSchool intern school', () => {
+    const James = new Intern("James", "5", "james@company.com", "University of Delaware");
+    expect(James.getSchool()).toBe("University of Delaware");
+})
